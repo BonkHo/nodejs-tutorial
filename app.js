55552487ba43a807479ff17c6b915a94a17f3891
John Bae <john21225@gmail.com>
2021-11-01 21:38:37 -0700

    Working with event emitters

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-const { readFile, writeFile } = require("fs");
-const util = require("util");
+const EventEmitter = require("events");
 
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const customEmitter = new EventEmitter();
 
-const start = async () => {
-	try {
-		const first = await readFilePromise("./content/first.txt", "utf-8");
-		const second = await readFilePromise("./content/second.txt", "utf-8");
-		await writeFilePromise(
-			"./content/result-mind-grenade.txt",
-			`This is the third file.`
-		);
-		const third = await readFilePromise(
-			"./content/result-mind-grenade.txt",
-			"utf-8"
-		);
-		console.log(first, second, third);
-	} catch (err) {
-		console.log(err);
-	}
-};
+customEmitter.on("response", () => {
+	console.log(`Data recieved.`);
+});
 
-start();
+customEmitter.emit("response");
